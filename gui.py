bcfe5b0f826c5a773bcf978f683320547501ddcd
Andrii Levchenko <82201855+Levchenko-A@users.noreply.github.com>
2024-08-21 21:20:04 +0200

    added QGraphics - future canvas for drawing

--- gui.py
+++ gui.py
@@ -1,15 +1,29 @@
 # -*- coding: utf-8 -*-
 
-from PySide6.QtCore import (QCoreApplication, QMetaObject, QRect)
-from PySide6.QtGui import (QAction)
-from PySide6.QtWidgets import (QApplication, QMenu, QMenuBar, QStatusBar, QWidget)
+################################################################################
+## Form generated from reading UI file 'gui_menubarcLRiYc.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
 
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
+from PySide6.QtWidgets import (QApplication, QGraphicsView, QMainWindow, QMenu,
+    QMenuBar, QSizePolicy, QStatusBar, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(600, 480)
+        MainWindow.resize(598, 478)
         self.actionOpen = QAction(MainWindow)
         self.actionOpen.setObjectName(u"actionOpen")
         self.actionSave = QAction(MainWindow)
@@ -34,10 +48,13 @@ class Ui_MainWindow(object):
         self.actionCoordinates_4.setObjectName(u"actionCoordinates_4")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.graphicsView = QGraphicsView(self.centralwidget)
+        self.graphicsView.setObjectName(u"graphicsView")
+        self.graphicsView.setGeometry(QRect(10, 10, 581, 421))
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 600, 22))
+        self.menubar.setGeometry(QRect(0, 0, 598, 22))
         self.menuFile = QMenu(self.menubar)
         self.menuFile.setObjectName(u"menuFile")
         self.menuGeometry = QMenu(self.menubar)
@@ -82,7 +99,6 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
 
         QMetaObject.connectSlotsByName(MainWindow)
-
     # setupUi
 
     def retranslateUi(self, MainWindow):
@@ -107,3 +123,4 @@ class Ui_MainWindow(object):
         self.menuLine_2.setTitle(QCoreApplication.translate("MainWindow", u"Line", None))
         self.menuEndpoints.setTitle(QCoreApplication.translate("MainWindow", u"Endpoints", None))
     # retranslateUi
+
